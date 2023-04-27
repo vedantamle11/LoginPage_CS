c8e1138a8f7de3bccef51a9a935e679e69389287
HP <HP@LAPTOP-8461GSRU>
2023-04-27 11:10:50 +0530

    Add project files.

---
+++ LoginPage/WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LoginPage.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 100px;
+        }
+        .auto-style3 {
+            width: 198px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table cellpadding="4" cellspacing="4" class="auto-style1">
+            <tr>
+                <td class="auto-style2">Username :</td>
+                <td>
+                    <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Password :</td>
+                <td>
+                    <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td>
+                    <asp:Button ID="BTNLOGIN" runat="server" Text="Login" />
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ LoginPage/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LoginPage
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection("Server=LAPTOP-8461GSRU;Database=user_info;Trusted_Connection=True");
+                SqlCommand command = new SqlCommand("SELECT * FROM Details WHERE userName=@userName AND passwords=@passwords", connection);
+
+                command.Parameters.AddWithValue("@userName", txtUsername.Text);
+                command.Parameters.AddWithValue("@passwords", txtPassword.Text);
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "Details");
+
+                if (ds.Tables["Details"].Rows.Count == 0)
+                {
+                    Response.Write("Invalid User");
+                }
+                else
+                {
+                    Response.Redirect("Dashboard.aspx");
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Response.Write("SQL Exception: " + ex.Message);
+            }
+        }
+    }
+}
